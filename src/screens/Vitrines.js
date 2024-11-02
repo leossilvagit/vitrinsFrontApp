063e8693f18c1f1840a7ee0fe6142f78a8f71753
Leonilson Silva <contatoleonilsonsantos@gmail.com>
2024-11-02 14:41:11 -0300

    new file:   src/assets/logos/belezafidalga.png 	new file:   src/assets/logos/belezaurbana.png 	new file:   src/assets/logos/bmlogo.png 	new file:   src/assets/logos/casafredy.png 	new file:   src/assets/logos/espacovitrine.png 	new file:   src/assets/logos/loshermanos.png 	new file:   src/assets/logos/maisduo.png 	new file:   src/assets/logos/paulaventura.png 	new file:   src/assets/logos/vilahair.png 	new file:   src/assets/produtos/Kitciliosmagneticos.png 	new file:   src/assets/produtos/Secador.png 	deleted:    src/assets/produtos/corona.png 	deleted:    src/assets/produtos/heneken.png 	new file:   src/assets/produtos/kitalongamento.png 	new file:   src/assets/produtos/kitcadiveu.png 	new file:   src/assets/produtos/kitcilios.png 	new file:   src/assets/produtos/lixadeiraeletrica.png 	deleted:    src/assets/produtos/spaten.png 	modified:   src/screens/Vitrines.js

--- src/screens/Vitrines.js
+++ src/screens/Vitrines.js
@@ -5,30 +5,68 @@ import VitrineCategoria from '../components/VitrineCategoria';
 import Produto from '../components/Produto';
 
 const destaques = [
-  { id: '1', titulo: 'Adega 24h', cor: '#FF6347', imagem: require('../assets/destaques/novidades.png') },
-  { id: '2', titulo: 'Descontão', cor: '#FFD700', imagem: require('../assets/destaques/ofertas.png') },
-  { id: '3', titulo: 'Só breja', cor: '#1E90FF', imagem: require('../assets/destaques/retornaveis.png') },
-  { id: '4', titulo: '3 por 10', cor: '#32CD32', imagem: require('../assets/destaques/latas.png') },
-  { id: '5', titulo: 'Chopp', cor: '#FFA07A', imagem: require('../assets/destaques/chopp.png') },
+  { id: '1', titulo: 'Beleza Fidalga', cor: '#FF6347', imagem: require('../assets/logos/belezafidalga.png') },
+  { id: '2', titulo: 'Beleza urbana', cor: '#FFD700', imagem: require('../assets/logos/belezaurbana.png') },
+  { id: '3', titulo: 'Paula Ventura', cor: '#1E90FF', imagem: require('../assets/logos/paulaventura.png') },
+  { id: '4', titulo: 'BM', cor: '#32CD32', imagem: require('../assets/logos/bmlogo.png') },
+  { id: '5', titulo: 'Mais Duo', cor: '#FFA07A', imagem: require('../assets/logos/maisduo.png') },
 ];
 
-// Dados de exemplo para categorias e produtos
 const categorias = [
-  { id: '2', categoria: 'Cervejas', imagem: require('../assets/categorias/cervejas.png') },
-  { id: '3', categoria: 'Destilados', imagem: require('../assets/categorias/destilados.png') },
-  { id: '4', categoria: 'Vinhos', imagem: require('../assets/categorias/vinhos.png') },
-  { id: '5', categoria: 'Chopp', imagem: require('../assets/categorias/chopp.png') },
-  { id: '6', categoria: 'Não alcoólicos', imagem: require('../assets/categorias/naoalcoolicos.png') },
-  { id: '7', categoria: 'Águas e Gelo', imagem: require('../assets/categorias/aguagelo.png') },
-  { id: '8', categoria: 'Comidinhas', imagem: require('../assets/categorias/comidinhas.png') },
-  { id: '9', categoria: 'Drinks Prontos', imagem: require('../assets/categorias/drinks.png') },
-  { id: '10', categoria: 'Artesanais', imagem: require('../assets/categorias/artesanais.png') },
+  { id: '2', categoria: 'BM', imagem: require('../assets/logos/bmlogo.png') },
+  { id: '3', categoria: 'Mais Duo', imagem: require('../assets/logos/maisduo.png') },
+  { id: '4', categoria: 'Beleza urbana', imagem: require('../assets/logos/belezaurbana.png') },
+  { id: '5', categoria: 'Vila Hair', imagem: require('../assets/logos/vilahair.png') },
+  { id: '6', categoria: 'Los Hermanos', imagem: require('../assets/logos/loshermanos.png') },
+  { id: '7', categoria: 'Espaço Vitrine', imagem: require('../assets/logos/espacovitrine.png') },
 ];
 
 const produtosFavoritos = [
-  { id: '1', nome: 'Cerveja Corona Extra', volume: '330ml', imagem:  require('../assets/produtos/corona.png')},
-  { id: '2', nome: 'Heineken', volume: '355ml', imagem: require('../assets/produtos/heneken.png')},
-  { id: '3', nome: 'Spaten', volume: '330ml', imagem: require('../assets/produtos/spaten.png') },
+  { id: '1', nome: 'Kit Secador 220w', imagem: require('../assets/produtos/Secador.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit Secador 220w', imagem: require('../assets/produtos/Secador.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/Kitciliosmagneticos.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/Kitciliosmagneticos.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Kit Silios Magneticos', imagem: require('../assets/produtos/Kitciliosmagneticos.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Kit Silios Magneticos', imagem: require('../assets/produtos/Kitciliosmagneticos.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Kit Silios Magneticos', imagem: require('../assets/produtos/Kitciliosmagneticos.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
+  { id: '1', nome: 'Kit alongamento', imagem: require('../assets/produtos/kitalongamento.png') },
+  { id: '2', nome: 'Lixadeira eletrica', imagem: require('../assets/produtos/lixadeiraeletrica.png') },
+  { id: '3', nome: 'Kit Cilios', imagem: require('../assets/produtos/kitcilios.png') },
   // Outros produtos...
 ];
 
@@ -37,8 +75,9 @@ const Vitrines = () => {
 
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.content}>
+      <ScrollView contentContainerStyle={styles.scrollContent}>
         <View style={styles.searchContainer}>
+        <Text style={styles.endereco}>Rua Victor Civicta, 60</Text>
           <TextInput
             style={styles.searchInput}
             placeholder="Pesquisar"
@@ -47,7 +86,6 @@ const Vitrines = () => {
           />
         </View>
 
-        <Text style={styles.sectionTitle}>Vitrines em Destaque</Text>
         <FlatList
           data={destaques}
           renderItem={({ item }) => (
@@ -64,7 +102,6 @@ const Vitrines = () => {
           contentContainerStyle={styles.destaqueContainer}
         />
 
-        <Text style={styles.sectionTitle}>Vitrines</Text>
         <FlatList
           data={categorias}
           renderItem={({ item }) => (
@@ -79,34 +116,62 @@ const Vitrines = () => {
           scrollEnabled={false}
         />
 
-        <Text style={styles.sectionTitle}>Produtos</Text>
-        <FlatList
-          data={produtosFavoritos}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          renderItem={({ item }) => (
-            <Produto nome={item.nome} volume={item.volume} imagem={item.imagem} preco={item.preco} />
-          )}
-          keyExtractor={(item) => item.id}
-        />
+        <Text style={styles.sectionTitle}>Produtos recomendados</Text>
+        // Primeira linha de produtos
+          <FlatList
+            data={produtosFavoritos.slice(0, 10)} // Primeira linha com 10 produtos
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <View style={styles.produtoContainer}>
+                <Image source={item.imagem} style={styles.produtoImagem} />
+                <Text style={styles.produtoNome}>{item.nome}</Text>
+                <Text style={styles.produtoVolume}>{item.volume}</Text>
+                <TouchableOpacity style={styles.adicionarButton}>
+                  <Text style={styles.adicionarButtonText}>Adicionar</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+
+          // Segunda linha de produtos
+          <FlatList
+            data={produtosFavoritos.slice(10, 20)} // Segunda linha com os próximos 10 produtos
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <View style={styles.produtoContainer}>
+                <Image source={item.imagem} style={styles.produtoImagem} />
+                <Text style={styles.produtoNome}>{item.nome}</Text>
+                <Text style={styles.produtoVolume}>{item.volume}</Text>
+                <TouchableOpacity style={styles.adicionarButton}>
+                  <Text style={styles.adicionarButtonText}>Adicionar</Text>
+                </TouchableOpacity>
+              </View>
+            )}
+            keyExtractor={(item) => item.id}
+          />
+
+
       </ScrollView>
 
       <View style={styles.bottomMenu}>
         <TouchableOpacity style={styles.menuItem}>
-          <Icon name="home" size={24} color="#333" />
-          <Text style={styles.menuItemText}>Home</Text>
+          <Icon name="home" size={24} color="#800080" />
+          <Text style={styles.menuItemText}>Vitrines</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.menuItem}>
-          <Icon name="search" size={24} color="#333" />
+          <Icon name="search" size={24} color="#800080" />
           <Text style={styles.menuItemText}>Buscar</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.menuItem}>
-          <Icon name="person" size={24} color="#333" />
+          <Icon name="person" size={24} color="#800080" />
           <Text style={styles.menuItemText}>Perfil</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.menuItem}>
-          <Icon name="settings" size={24} color="#333" />
-          <Text style={styles.menuItemText}>Configurações</Text>
+          <Icon name="receipt" size={24} color="#800080" />
+          <Text style={styles.menuItemText}>Pedidos</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -118,12 +183,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  content: {
-    flex: 1,
+  scrollContent: {
+    paddingBottom: 80,
   },
   searchContainer: {
     padding: 16,
     backgroundColor: 'white',
+    marginTop: 15,
+    marginBottom: 15,
   },
   searchInput: {
     backgroundColor: '#f5f5f5',
@@ -136,6 +203,11 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     elevation: 2,
   },
+  endereco: {
+    textAlign: 'center',
+    color: 'black',
+    marginBottom: 10,
+  },
   sectionTitle: {
     fontSize: 16,
     fontWeight: 'bold',
@@ -146,6 +218,7 @@ const styles = StyleSheet.create({
   },
   destaqueContainer: {
     paddingHorizontal: 16,
+    marginBottom: 40,
   },
   destaqueWrapper: {
     alignItems: 'center',
@@ -158,11 +231,13 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     justifyContent: 'center',
     alignItems: 'center',
+    borderWidth: 3,
+    borderColor: '#800080',
   },
   destaqueImage: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 40,
+    width: 70,
+    height: 70,
+    borderRadius: 37.5,
   },
   destaqueText: {
     fontSize: 12,
@@ -186,13 +261,25 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     paddingHorizontal: 16,
   },
+  horizontalFlatList: {
+    marginVertical: 8,
+  },
   bottomMenu: {
+    position: 'absolute',
+    bottom: 20, 
+    left: '20%',
+    right: '20%',
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-evenly',
     paddingVertical: 10,
+    paddingHorizontal: 15,
     backgroundColor: '#f8f8f8',
-    borderTopWidth: 1,
-    borderColor: '#ddd',
+    borderRadius: 30,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 4,
+    elevation: 5,
   },
   menuItem: {
     alignItems: 'center',
@@ -200,7 +287,44 @@ const styles = StyleSheet.create({
   menuItemText: {
     fontSize: 10,
     color: '#333',
-    marginTop: 0,
+    marginTop: 2,
+  },
+  produtoContainer: {
+    alignItems: 'center',
+    margin: 8,
+    width: 120,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 1,
+    elevation: 5,
+  },
+  produtoImagem: {
+    width: 80,
+    height: 80,
+    marginBottom: 8,
+  },
+  produtoNome: {
+    fontSize: 12,
+    textAlign: 'center',
+    marginBottom: 4,
+  },
+  produtoVolume: {
+    fontSize: 10,
+    color: '#555',
+    marginBottom: 8,
+  },
+  adicionarButton: {
+    backgroundColor: '#800080', // Cor roxa
+    borderRadius: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  adicionarButtonText: {
+    color: 'white',  
   },
 });
 
